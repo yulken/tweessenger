25e0cf2183d778f7e0e58ad87661514c04151b89
Caio Torres <caiocftorres@gmail.com>
2019-11-10 20:31:58 -0300

    Working version of Twitter data mining

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from sender import Sender
-from config import TELEGRAM_API_TOKEN, TELEGRAM_API_CHAT_ID
+from twitter_miner import TwitterMiner
+import config as cfg
+
+ids = {
+	'SUPERVIA_TWITTER': '76395804',
+}
+
+
+def get_supervia_status():
+	keyword = 'Belford Roxo'
+	last_tweet = TwitterMiner.get_last_tweet_id(ids['SUPERVIA_TWITTER'], path)
+	msg_list = tm.search_for(ids['SUPERVIA_TWITTER'], last_tweet, keyword)
+
+	for msg in msg_list:
+		sender.send_message(msg)
+
+	TwitterMiner.update_last_tweet_id(ids['SUPERVIA_TWITTER'], tm, path)
 
 
 if __name__ == '__main__':
-	m = Sender(TELEGRAM_API_TOKEN, TELEGRAM_API_CHAT_ID)
-	m.send_message("Olá, eu sou um bot do Telegram")
+	tm = TwitterMiner(
+		key=cfg.TWITTER_API_KEY, key_secret=cfg.TWITTER_API_KEY_SECRET,
+		token=cfg.TWITTER_API_TOKEN, token_secret=cfg.TWITTER_API_TOKEN_SECRET
+	)
+	sender = Sender(cfg.TELEGRAM_API_TOKEN, cfg.TELEGRAM_API_CHAT_ID)
+	path = 'last-id.log'
+	get_supervia_status()
---
+++ twitter_miner.py
@@ -0,0 +1,70 @@
+import tweepy
+import re
+
+
+class TwitterMiner:
+	def __init__(self, key, key_secret, token, token_secret):
+		auth = tweepy.OAuthHandler(
+			consumer_key=key,
+			consumer_secret=key_secret
+		)
+		auth.set_access_token(
+			key=token,
+			secret=token_secret
+		)
+		self.api = tweepy.API(
+			auth,
+			parser=tweepy.parsers.JSONParser()
+		)
+
+	@staticmethod
+	def get_last_tweet_id(target_id, path):
+		try:
+			f = open(path, 'r')
+			for line in reversed(f.readlines()):
+				if re.search(target_id, line):
+					last_tweet = line.split(';')[1]
+					return int(last_tweet)
+
+			f.close()
+		except FileNotFoundError:
+			print("File not found")
+
+	@staticmethod
+	def update_last_tweet_id(target_id, twitter_miner, path):
+		last_tweet = twitter_miner.get_last_remote_tweet_id(target_id)
+		if last_tweet != TwitterMiner.get_last_tweet_id(target_id, path):
+			f = open(path, 'a+')
+			f.write(f'{target_id};{last_tweet}\n')
+			f.close()
+
+	def get_last_remote_tweet_id(self, target_id):
+		tl = self.api.user_timeline(
+			id=target_id,
+			tweet_mode="extended",
+			count=1
+		)
+		return tl[0]['id']
+
+	def search_for(self, target_id, last_tweet_id, keyword):
+		if last_tweet_id is None:
+			tl = self.api.user_timeline(
+				id=target_id,
+				tweet_mode="extended",
+			)
+		else:
+			tl = self.api.user_timeline(
+				id=target_id,
+				tweet_mode="extended",
+				since_id=last_tweet_id
+			)
+		msg_list = []
+		for tweet in tl:
+			msg = ''
+			if re.search(keyword, tweet['full_text'], re.IGNORECASE):
+				msg += f'<b>Message from {tweet["user"]["name"]}</b>:\n'
+				msg += f'{tweet["full_text"]}\n'
+				msg += f'https://twitter.com/user/status/{tweet["id"]}'
+				msg_list.append(msg)
+		return msg_list
+
