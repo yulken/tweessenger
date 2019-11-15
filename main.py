5f4b5578f2d79638c6c487be66cd3fd9a091c9a9
Caio Torres <caiocftorres@gmail.com>
2019-11-14 23:49:26 -0300

    Printing begin and end of operation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sender import Sender
 from twitter_miner import TwitterMiner
 import config as cfg
+from datetime import datetime
 
 ids = {
 	'SUPERVIA_TWITTER': '76395804',
@@ -24,6 +25,8 @@ def get_supervia_status():
 
 
 if __name__ == '__main__':
+	ts1 = datetime.now()
+	print(f'{ts1.strftime("%Y/%m/%d_%H:%M:%S")} - Starting')
 	tm = TwitterMiner(
 		key=cfg.TWITTER_API_KEY, key_secret=cfg.TWITTER_API_KEY_SECRET,
 		token=cfg.TWITTER_API_TOKEN, token_secret=cfg.TWITTER_API_TOKEN_SECRET
@@ -31,3 +34,5 @@ if __name__ == '__main__':
 	sender = Sender(cfg.TELEGRAM_API_TOKEN, cfg.TELEGRAM_API_CHAT_ID)
 	path = 'last-id.log'
 	get_supervia_status()
+	ts2 = datetime.now()
+	print(f'{ts2.strftime("%Y/%m/%d_%H:%M:%S")} - Done')
